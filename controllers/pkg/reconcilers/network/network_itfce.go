d9ba4a4c1f64928223b08e9d6ab00d6c5ab875c3
Wim Henderickx <wim.henderickx@gmail.com>
2023-05-30 07:14:16 +0200

    added routed interface vlan handling

--- controllers/pkg/reconcilers/network/network_itfce.go
+++ controllers/pkg/reconcilers/network/network_itfce.go
@@ -68,18 +68,20 @@ func (r *network) PopulateBridgeInterface(ctx context.Context, cr *infrav1alpha1
 	ifName = strings.ReplaceAll(ifName, "e", "ethernet-")
 	niItfceSubItfceName := strings.Join([]string{ifName, strconv.Itoa(int(vlanId))}, ".")
 	i := r.devices[nodeName].GetOrCreateInterface(ifName)
-	if attachmentType == reqv1alpha1.AttachmentTypeVLAN {
-		i.VlanTagging = ygot.Bool(true)
-	}
+
 	si := i.GetOrCreateSubinterface(uint32(vlanId))
 	si.Type = ygotsrl.SrlNokiaInterfaces_SiType_bridged
-	si.Vlan = &ygotsrl.SrlNokiaInterfaces_Interface_Subinterface_Vlan{
-		Encap: &ygotsrl.SrlNokiaInterfaces_Interface_Subinterface_Vlan_Encap{
-			SingleTagged: &ygotsrl.SrlNokiaInterfaces_Interface_Subinterface_Vlan_Encap_SingleTagged{
-				VlanId: ygotsrl.UnionUint16(vlanId),
+	if attachmentType == reqv1alpha1.AttachmentTypeVLAN {
+		i.VlanTagging = ygot.Bool(true)
+		si.Vlan = &ygotsrl.SrlNokiaInterfaces_Interface_Subinterface_Vlan{
+			Encap: &ygotsrl.SrlNokiaInterfaces_Interface_Subinterface_Vlan_Encap{
+				SingleTagged: &ygotsrl.SrlNokiaInterfaces_Interface_Subinterface_Vlan_Encap_SingleTagged{
+					VlanId: ygotsrl.UnionUint16(vlanId),
+				},
 			},
-		},
+		}
 	}
+	
 	ni := r.devices[nodeName].GetOrCreateNetworkInstance(bdName)
 	ni.GetOrCreateInterface(niItfceSubItfceName)
 	return nil
@@ -114,18 +116,19 @@ func (r *network) PopulateRoutedInterface(ctx context.Context, cr *infrav1alpha1
 	ifName = strings.ReplaceAll(ifName, "e", "ethernet-")
 	niItfceSubItfceName := strings.Join([]string{ifName, strconv.Itoa(int(vlanId))}, ".")
 	i := r.devices[nodeName].GetOrCreateInterface(ifName)
-	if attachmentType == reqv1alpha1.AttachmentTypeVLAN {
-		i.VlanTagging = ygot.Bool(true)
-	}
 
 	si := i.GetOrCreateSubinterface(uint32(vlanId))
 	si.Type = ygotsrl.SrlNokiaInterfaces_SiType_routed
-	si.Vlan = &ygotsrl.SrlNokiaInterfaces_Interface_Subinterface_Vlan{
-		Encap: &ygotsrl.SrlNokiaInterfaces_Interface_Subinterface_Vlan_Encap{
-			SingleTagged: &ygotsrl.SrlNokiaInterfaces_Interface_Subinterface_Vlan_Encap_SingleTagged{
-				VlanId: ygotsrl.UnionUint16(vlanId),
+	
+	if attachmentType == reqv1alpha1.AttachmentTypeVLAN {
+		i.VlanTagging = ygot.Bool(true)
+		si.Vlan = &ygotsrl.SrlNokiaInterfaces_Interface_Subinterface_Vlan{
+			Encap: &ygotsrl.SrlNokiaInterfaces_Interface_Subinterface_Vlan_Encap{
+				SingleTagged: &ygotsrl.SrlNokiaInterfaces_Interface_Subinterface_Vlan_Encap_SingleTagged{
+					VlanId: ygotsrl.UnionUint16(vlanId),
+				},
 			},
-		},
+		}
 	}
 
 	for _, prefix := range prefixes {
@@ -188,45 +191,42 @@ func (r *network) PopulateRoutedInterface(ctx context.Context, cr *infrav1alpha1
 				allocv1alpha1.NephioNsnNamespaceKey: cr.Namespace,
 			}
 
-			if prefixKind == ipamv1alpha1.PrefixKindNetwork {
-				prefixAlloc, err := r.IpamClientProxy.Allocate(ctx, ipamv1alpha1.BuildIPAllocation(
-					metav1.ObjectMeta{
-						Name:      fmt.Sprintf("%s-%s", LinkName, nodeName),
-						Namespace: cr.Namespace,
-					},
-					ipamv1alpha1.IPAllocationSpec{
-						Kind:            ipamv1alpha1.PrefixKindNetwork,
-						NetworkInstance: corev1.ObjectReference{Name: rtName, Namespace: cr.Namespace},
-						AddressFamily:   &af,
-						AllocationLabels: allocv1alpha1.AllocationLabels{
-							UserDefinedLabels: allocv1alpha1.UserDefinedLabels{
-								Labels: map[string]string{
-									allocv1alpha1.NephioGatewayKey: "true",
-								},
-							},
-							Selector: &metav1.LabelSelector{
-								MatchLabels: addressSelectorLabels,
+			prefixAlloc, err := r.IpamClientProxy.Allocate(ctx, ipamv1alpha1.BuildIPAllocation(
+				metav1.ObjectMeta{
+					Name:      fmt.Sprintf("%s-%s", LinkName, nodeName),
+					Namespace: cr.Namespace,
+				},
+				ipamv1alpha1.IPAllocationSpec{
+					Kind:            ipamv1alpha1.PrefixKindNetwork,
+					NetworkInstance: corev1.ObjectReference{Name: rtName, Namespace: cr.Namespace},
+					AddressFamily:   &af,
+					AllocationLabels: allocv1alpha1.AllocationLabels{
+						UserDefinedLabels: allocv1alpha1.UserDefinedLabels{
+							Labels: map[string]string{
+								allocv1alpha1.NephioGatewayKey: "true",
 							},
 						},
+						Selector: &metav1.LabelSelector{
+							MatchLabels: addressSelectorLabels,
+						},
 					},
-					ipamv1alpha1.IPAllocationStatus{},
-				), nil)
-				if err != nil {
-					return err
-				}
-				if pi.IsIpv6() {
-					ipv6 := si.GetOrCreateIpv6()
-					ipv6.AppendAddress(&ygotsrl.SrlNokiaInterfaces_Interface_Subinterface_Ipv6_Address{
-						IpPrefix: prefixAlloc.Status.Prefix,
-					})
-				} else {
-					ipv4 := si.GetOrCreateIpv4()
-					ipv4.AppendAddress(&ygotsrl.SrlNokiaInterfaces_Interface_Subinterface_Ipv4_Address{
-						IpPrefix: prefixAlloc.Status.Prefix,
-					})
-				}
+				},
+				ipamv1alpha1.IPAllocationStatus{},
+			), nil)
+			if err != nil {
+				return err
 			}
-			si.GetOrCreateAnycastGw().VirtualRouterId = ygot.Uint8(1)
+			if pi.IsIpv6() {
+				ipv6 := si.GetOrCreateIpv6()
+				ipv6.AppendAddress(&ygotsrl.SrlNokiaInterfaces_Interface_Subinterface_Ipv6_Address{
+					IpPrefix: prefixAlloc.Status.Prefix,
+				})
+			} else {
+				ipv4 := si.GetOrCreateIpv4()
+				ipv4.AppendAddress(&ygotsrl.SrlNokiaInterfaces_Interface_Subinterface_Ipv4_Address{
+					IpPrefix: prefixAlloc.Status.Prefix,
+				})
+			}
 		}
 	}
 
